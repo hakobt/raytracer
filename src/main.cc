6b4036fdf3f0b762605c5dc807f24d871e1c4bf9
Hakob <hakobtovmasyan19@gmail.com>
2024-06-13 08:44:17 +0400

    gitignore

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 #include "color.h"
 #include <iostream>
 
-color ray_color(const ray& r) {
+color ray_color(const ray &r) {
     return color(0, 0, 0);
 }
 
@@ -12,7 +12,7 @@ int main() {
 
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
-    
+
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
@@ -20,9 +20,9 @@ int main() {
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
-    
+
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
     auto viewport_u = vec3(viewport_width, 0, 0);
     auto viewport_v = vec3(0, -viewport_height, 0);
@@ -32,30 +32,29 @@ int main() {
     auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
-            
+
             ray r(camera_center, ray_direction);
-            
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            
+
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+
             auto pixel_color = color(r, g, 0);
             write_color(std::cout, pixel_color);
         }
     }
-    
+
     std::clog << "Done.";
-    
 }
--- src/ray.h
+++ src/ray.h
@@ -4,20 +4,20 @@
 #include "vec3.h"
 
 class ray {
-  public:
-    ray() {}
-    ray(const point3& origin, const vec3& direction): orig(origin), dir(direction) {}
+public:
+  ray() {}
+  ray(const point3 &origin, const vec3 &direction) : orig(origin), dir(direction) {}
+
+  const point3 &origin() const { return orig; }
+  const vec3 &dir() const { return dir; }
+
+  point3 at(double t) const {
+    return origin + t * dir;
+  }
     
-    const point3& origin() const { return orig; }
-    const vec3& dir() const { return dir; }
-    
-    point3 at(double t) const {
-      return origin + t*dir;
-    }
-    
-  private:
-    point3 orig;
-    vec3 dir;
+private:
+  point3 orig;
+  vec3 dir;
 };
 
 #endif
